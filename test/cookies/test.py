7c0125bc5042b079a10985d3d0312ca88999cc94
9527zxl <1127580235@qq.com>
2022-02-14 20:03:28 +0800

    cs

--- test/cookies/test.py
+++ test/cookies/test.py
@@ -4,9 +4,15 @@ with open('cookeis.json', 'r') as f:
     # 使用json读取cookies 注意读取的是文件 所以用load而不是loads
     cookies_list = json.load(f)
 
-    cookie = ''
-    for id in cookies_list:
-        text = id['name'] + '=' + id['value'] + ';'
-        cookie = text
-        # print(cookie)
-    print(cookie)
+    # for id in cookies_list:
+        # print(id['name'])
+
+    print(cookies_list[1]['name'])
+    # cookie = ''
+    # for id in cookies_list:
+    #     text = id['name'] + '=' + id['value'] + ';'
+    #     cookie += text
+    #     cookie += "\n"
+    #     # cookie.join(text)
+    #     # print(text)
+    # print(cookie)

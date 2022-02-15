f71f56dcd90178f09b72628dfbb1bcfe8812c69f
9527zxl <1127580235@qq.com>
2022-02-16 00:39:19 +0800

    搭建框架已及编写工具

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-from time import sleep
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
+from tool.tool import getdriver
 
 if __name__ == '__main__':
-    options = Options()
-    # chrome.exe --remote-debugging-port=9527 --user-data-dir=“D:\Chrome”
-    options.add_experimental_option("debuggerAddress", "127.0.0.1:9527")
-    bro = webdriver.Chrome(options=options)
-
-    bro.get('http://cpquery.cnipa.gov.cn/')
-    bro.implicitly_wait(20)
-    sleep(3)
-
-    bro.find_element(By.XPATH, '//input[@id="username1"]').send_keys('15755188511')
-    bro.find_element(By.XPATH, '//input[@id="password1"]').send_keys('Zhixin888*')
-
-    input()
-
-    bro.find_element(By.XPATH, '//input[@id="publiclogin"]').click()
-    cookis = bro.get_cookies()
-    print(cookis)
+    driver = getdriver()
+    driver.get('http://cpquery.cnipa.gov.cn/')
---
+++ tool/tool.py
@@ -0,0 +1,57 @@
+import json
+
+import muggle_ocr
+from PIL import Image
+from selenium.webdriver import Firefox
+from selenium.webdriver import FirefoxOptions
+from selenium.webdriver.common.by import By
+
+
+def getdriver():
+    options = FirefoxOptions()
+    # options.add_argument('--headless')  # 无头浏览器
+    driver_path = 'D:\PythonWarehouse\FeiBiaoAutomation\driver\geckodriver.exe'
+    driver = Firefox(executable_path=driver_path, options=options)
+
+    return driver
+
+
+# 登录飞镖网后台获取cookie值
+def feibiao_login(username, password):
+    driver = getdriver()
+    driver.get('http://www.ipfeibiao.com/manager/frame/index')
+    driver.maximize_window()
+    driver.implicitly_wait(20)
+
+    # 定位元素输入账号密码
+    driver.find_element(By.XPATH, "//input[@name='user_name']").send_keys(username)
+    driver.find_element(By.XPATH, "//input[@name='password']").send_keys(password)
+
+    # 获取验证码
+    driver.save_screenshot('./verification_code/FeiBiaoLogin.png')
+    imgelement = driver.find_element(By.XPATH, '//*[@id="img_captcha"]')  # 定位验证码
+    location = imgelement.location  # 获取验证码x,y轴坐标
+    size = imgelement.size  # 获取验证码的长宽
+    rangle = (int(location['x']), int(location['y']), int(location['x'] + size['width']),
+              int(location['y'] + size['height']))  # 写成我们需要截取的位置坐标
+    i = Image.open("./verification_code/FeiBiaoLogin.png")  # 打开截图
+    frame4 = i.crop(rangle)  # 使用Image的crop函数，从截图中再次截取我们需要的区域
+    frame4.save('./verification_code/code.png')  # 保存我们接下来的验证码图片 进行打码
+
+    # 识别验证码
+    sdk = muggle_ocr.SDK(model_type=muggle_ocr.ModelType.Captcha)
+    with open(r"./verification_code/code.png", "rb") as f:
+        img = f.read()
+    code = sdk.predict(image_bytes=img)
+
+    print(code)
+
+    driver.find_element(By.XPATH, "//input[@name='captcha']").send_keys(code)
+
+    driver.find_element(By.XPATH, "//input[@class='loginin']").click()
+
+    cookie_text = json.dumps(driver.get_cookies())
+
+
+if __name__ == '__main__':
+    print()
